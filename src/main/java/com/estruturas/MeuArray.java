44729468154398ef2166c4ba68e5a5cf42d8f3a4
Rafael <rafael.felix.moraes@gmail.com>
2025-03-15 20:23:05 -0300

    feat: adicionar exemplo Array

---
+++ src/main/java/com/estruturas/MeuArray.java
@@ -0,0 +1,15 @@
+package com.estruturas;
+/**
+ * Classe para demonstrar o uso de Arrays.
+ */
+public class MeuArray {
+    /**
+     * Exemplo de Array de inteiros.
+     */
+    public void tipoArray(){
+        int[] numeros = {1, 2, 3, 4, 5};
+        for (int numero : numeros){
+            System.out.println(numero);
+        }
+    }
+}
