50cfdc818f16cd61b15cd6449a3803cf3233d3fa
Rafael <rafael.felix.moraes@gmail.com>
2025-03-15 20:34:20 -0300

    refactor: adicionar objetos para visualizar exemplos

--- src/main/java/com/estruturas/Main.java
+++ src/main/java/com/estruturas/Main.java
@@ -1,7 +1,35 @@
-package com.locadora;
+package com.estruturas;
 
+/**
+ * Classe principal.
+ */
 public class Main {
+    /**
+     * Método principal onde são executados os exemplos.
+     */
     public static void main(String[] args) {
+        System.out.println("Array");
+        new MeuArray().tipoArray();
+        System.out.println("----");
 
+        System.out.println("ArrayList");
+        new MinhaLista().tipoLista();
+        System.out.println("----");
+
+        System.out.println("Queue");
+        new MinhaFila().tipoFila();
+        System.out.println("----");
+
+        System.out.println("Stack");
+        new MinhaPilha().tipoPilha();
+        System.out.println("----");
+
+        System.out.println("Set");
+        new MeuConjunto().tipoConjunto();
+        System.out.println("----");
+
+        System.out.println("Map");
+        new MeuMap().tipoMap();
+        System.out.println("----");
     }
 }
