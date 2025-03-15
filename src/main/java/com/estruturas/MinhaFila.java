da0f3fd5c04b8a068aa9d13bcf354b7497ae61de
Rafael <rafael.felix.moraes@gmail.com>
2025-03-15 20:28:10 -0300

    feat: adicionar exemplo Queue

--- src/main/java/com/estruturas/MinhaFila.java
+++ src/main/java/com/estruturas/MinhaFila.java
@@ -1,2 +1,25 @@
-package com.estruturas;public class MinhaFila {
+package com.estruturas;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Classe para demonstrar o uso de filas (Queue).
+ */
+public class MinhaFila {
+    /**
+     * Exemplo de fila de inteiros.
+     */
+    public void tipoFila(){
+        Queue<Integer> fila = new LinkedList<>();
+        fila.add(10);
+        fila.add(20);
+        fila.add(30);
+        fila.add(40);
+        fila.add(50);
+
+        while (!fila.isEmpty()){
+            System.out.println(fila.poll());
+        }
+    }
 }
